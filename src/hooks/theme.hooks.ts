4650eb29e8a1ed72aed348dfe87cab833d635569
manongguai <jgbzql@163.com>
2023-08-29 10:55:36 +0800

    feat: usetheme优化

--- src/hooks/theme.hooks.ts
+++ src/hooks/theme.hooks.ts
@@ -1,6 +1,6 @@
 import { GlobalTheme } from '@/store/interface'
 import { useAppSelector } from './redux.hooks'
-import { useEffect } from 'react'
+import { CSSProperties, useEffect } from 'react'
 export function useTheme() {
   const { weakOrGray, theme, primary } = useAppSelector(
     (state) => state.global.themeConfig
@@ -11,12 +11,18 @@ export function useTheme() {
     )
     const body = document.documentElement as HTMLElement
     if (!weakOrGray) body.setAttribute('style', '')
-    if (weakOrGray === 'weak') body.setAttribute('style', 'filter: invert(80%)')
+    if (weakOrGray === 'weak')
+      body.setAttribute(
+        'style',
+        'filter: invert(80%);--primary-color:' + primary
+      )
     if (weakOrGray === 'gray')
-      body.setAttribute('style', 'filter: grayscale(1)')
+      body.setAttribute(
+        'style',
+        'filter: grayscale(1);--primary-color:' + primary
+      )
     document
       .querySelector('html')!
       .setAttribute('data-theme', theme == 'dark' ? 'dark' : '')
-    body.setAttribute('style', '--primary-color: ' + primary)
   }, [theme, weakOrGray, primary])
 }
