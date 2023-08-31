57a7353ff8e63e322b6e34f3f751adcbffdf6986
kirk <jgbzql@163.com>
2023-08-31 12:57:05 +0800

    feat: 用户详情页

--- src/views/User/detail.tsx
+++ src/views/User/detail.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Button, Card, Descriptions } from 'antd'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function detail() {
-  return <div>userDetail</div>
+  const { id } = useParams()
+  const navigate = useNavigate()
+  return (
+    <Card
+      style={{ height: '100%' }}
+      title="用户详情"
+      extra={<Button onClick={() => navigate(-1)}>返回</Button>}
+    >
+      <Descriptions>
+        <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+        <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+        <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+        <Descriptions.Item label="Remark">empty</Descriptions.Item>
+        <Descriptions.Item label="Address">
+          No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+        </Descriptions.Item>
+      </Descriptions>
+    </Card>
+  )
 }
 
 export default detail
